de536306a6b0344980386a964a54da8488a3959a
ningfan <1410160066@qq.com>
2024-03-21 09:07:40 +0800

    feat: 路由重定向逻辑优化

--- src/permission.ts
+++ src/permission.ts
@@ -24,8 +24,8 @@ router.beforeEach(async (to, from, next) => {
     /* 初始化全局字典 */
     // await dictionaryStore.initDictionary()
 
-    if (to.path === '/login') {
-      next({ path: '/' })
+    if (to.path === '/login' || to.path === '/') {
+      next({ path: permissionStore.addRouters[0].path })
     } else {
       if (permissionStore.getIsAddRouters) {
         next()
